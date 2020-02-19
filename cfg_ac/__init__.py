c980aa56590ed86bab3a7ee51430f285403128a7
Bruce Orcutt <cherveny@gmail.com>
2020-02-19 14:00:04 -0600

    movedFiles

--- cfg_ac/__init__.py
+++
@@ -1,5 +0,0 @@
-from  ac_files     import *
-from  ac_emails    import *
-from  ac_doctempl  import *
-from  ac_misc      import *
-from  ac_emailtmpl import *
--- finefee-query-wraper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LD_LIBRARY_PATH="/oracle/app/oracle/product/12.1.0.2/db_1/lib"
-
-/m1/incoming/vbin/autocirc/finefeequery.py
-
-
